40ed6e5127c5ad2d98ce2bcd3699f1a67cac525e
laszloscheers <laszloscheers.gmail.com>
2024-10-24 17:41:39 +0200

    CORS enabled for front-end

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: "https://football-hub.212-227-83-162.plesk.page/",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DeLETE,OPTIONS',
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 
